32cda52501c44cbf385d79f6b9db242fcefb77db
Maryan Vyntu <98353689+MarVyntu@users.noreply.github.com>
2023-06-22 06:45:06 +0000

    update homepage

--- prisma/migrations/20230622064056_checklist/migration.sql
+++ prisma/migrations/20230622064056_checklist/migration.sql
@@ -1,11 +1,10 @@
 -- CreateTable
 CREATE TABLE "Checklist" (
     "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "complete" BOOLEAN NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "complete" BOOLEAN NOT NULL,
 
     CONSTRAINT "Checklist_pkey" PRIMARY KEY ("id")
 );
-
