33702cf70110fc7746de098b50840f0fc9a7a297
Maryan Vyntu <98353689+MarVyntu@users.noreply.github.com>
2023-06-27 06:18:11 +0000

    add styles homepage

--- components/styles.ts
+++ components/styles.ts
@@ -12,6 +12,11 @@ const styles = {
     "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
   sectionSubText:
     "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+
+    BackroundDropMenu: "absolute right-5 bg-black bg-opacity-80 px-6 py-2 mt-1.5",
+
+    buttonsDropMenu: 
+      "border border-white  bg-black bg-opacity-80 px-3 py-0.5 rounded-xl my-2 mx-2"
 };
 
 export { styles };
