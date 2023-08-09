1fb7a3ae330c92ae6b89de05231529d067716433
Svyatoslav <blackdbf@gmail.com>
2023-08-09 12:16:48 +0300

    feat: check branch

--- components/MarkdownEditor.tsx
+++ components/MarkdownEditor.tsx
@@ -26,9 +26,9 @@ const MarkdownEditor = () => {
     return (
         <div className="flex flex-col gap-y-5">
             {isMarkVisible ? <MDEditor value={value} onChange={handleChange} /> : <h1>{value}</h1>}
-            <button className="px-4 py-2 rounded-full bg-green-500" onClick={handleEdit}>
+            {/* <button className="px-4 py-2 rounded-full bg-green-500" onClick={handleEdit}>
                 {isMarkVisible ? 'Save' : 'Edit'}
-            </button>
+            </button> */}
         </div>
     )
 }
